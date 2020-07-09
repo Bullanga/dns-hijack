a43ea5f5ecba1baf209ce9e494e04f506084571a
1227311 <miquel.roset@upc.edu>
2020-07-09 19:06:09 +0000

    add ipv4_list_contains and ipv4_list_clear

--- src/include/ipv4_list.h
+++ src/include/ipv4_list.h
@@ -2,11 +2,10 @@
 	#define IPV4_LIST_H
 
 	/* exported functions */
-	void *ipv4_list_create(const char *ip_first, const char *ip_last);
+	void *ipv4_list_create(char *ip_first, char *ip_last);
 	void  ipv4_list_clear(void *ipv4_list);
 	int   ipv4_list_add(void *ipv4_list, char *ip);
 	int   ipv4_list_contains(void  *ipv4_list, char *ip);
-
 
     void ipv4_list_debug(void *ipv4_list);
 
--- src/ipv4_list.c
+++ src/ipv4_list.c
@@ -13,26 +13,26 @@ typedef struct{
 	uint8_t *presence;
 } ipv4_list;
 
-void *ipv4_list_create(const char *ip_first, const char *ip_last) {
-    struct in_addr addr_n_first;
-    struct in_addr addr_n_last;
+uint32_t ip_to_n(uint32_t offset, char *ip) {
+    struct in_addr addr_n;
 
+    // Get value of ip
+    if (inet_aton(ip, &addr_n) == 0) {
+        fprintf(stderr, "Invalid address\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Return index relative to offset
+    return ntohl(addr_n.s_addr) - offset;
+}
+
+void *ipv4_list_create(char *ip_first, char *ip_last) {
     // Allocate memory for the struct
 	ipv4_list *list = malloc(sizeof(ipv4_list));
 
-    // Get first and last address in network byte order
-    if (inet_aton(ip_first, &addr_n_first) == 0) {
-        fprintf(stderr, "Invalid address\n");
-        exit(EXIT_FAILURE);
-    }
-    if (inet_aton(ip_last, &addr_n_last) == 0) {
-        fprintf(stderr, "Invalid address\n");
-        exit(EXIT_FAILURE);
-    }
-
     // Fill struct
-    list->ip_first_n = ntohl(addr_n_first.s_addr);
-    list->list_size = ntohl(addr_n_last.s_addr) - list->ip_first_n + 1;
+    list->ip_first_n = ip_to_n(0, ip_first);
+    list->list_size = ip_to_n(0, ip_last) - list->ip_first_n + 1;
 
     list->presence = malloc(list->list_size*sizeof(uint8_t));
     memset(list->presence, 0, list->list_size*sizeof(uint8_t));
@@ -42,27 +42,37 @@ void *ipv4_list_create(const char *ip_first, const char *ip_last) {
 
 void ipv4_list_clear(void *_list) {
     ipv4_list * list = _list;
+
+    // Set to 0 all positions from presence vector.
     memset(list->presence, 0, list->list_size*sizeof(uint8_t));
 }
 
 int ipv4_list_add(void *_list, char *ip) {
     uint32_t index;
-    struct in_addr addr_n;
     ipv4_list *list = _list;
     int return_value = 0;
 
-    // Get value of ip and calculate its index in the precence vector
-    if (inet_aton(ip, &addr_n) == 0) {
-        fprintf(stderr, "Invalid address\n");
-        exit(EXIT_FAILURE);
-    }
-    index = ntohl(addr_n.s_addr) - list->ip_first_n;
+    index = ip_to_n(list->ip_first_n, ip);
 
     // Check if its a valid index and add it if so
     if (index >= 0 && index < list->list_size) {
-        list->presence[index]  =  1;
-        return_value      =  1;
+        list->presence[index] = 1;
+        return_value = 1;
     }
+
+    return return_value;
+}
+
+int ipv4_list_contains(void *_list, char *ip) {
+    uint32_t index;
+    int return_value = 0;
+    ipv4_list *list = _list;
+
+    index = ip_to_n(list->ip_first_n, ip);
+
+    // Check if its a valid index and add it if so
+    if (index >= 0 && index < list->list_size && list->presence[index])
+        return_value = 1;
 
     return return_value;
 }
