cd248c20ac2ca609dd665a069dcb6735491c311f
1227311 <miquel.roset@upc.edu>
2020-07-10 18:14:07 +0000

    add comments to .h ipv4 file

--- src/include/ipv4_list.h
+++ src/include/ipv4_list.h
@@ -2,11 +2,20 @@
 	#define IPV4_LIST_H
 
 	/* exported functions */
+
+    // Returns a pointer to ipv4_list object
 	void *ipv4_list_create(char *ip_first, char *ip_last);
+
+    // Remove all elements from the list container
 	void  ipv4_list_clear(void *ipv4_list);
+
+    // Returns 1 if the ip was successfully added, 0 otherwise
 	int   ipv4_list_add(void *ipv4_list, char *ip);
+
+    // Returns 1 if the list contains the given ip, 0 otherwise
 	int   ipv4_list_contains(void  *ipv4_list, char *ip);
 
+    // Prints to stdout information about the list object
     void ipv4_list_debug(void *ipv4_list);
 
 #endif
