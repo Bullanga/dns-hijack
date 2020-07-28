e43715f8a9ab1204373b6d5c49a5d2ef8b80e3e1
dhap0 <dhap0.infosec@gmail.com>
2020-07-28 23:26:13 +0200

    neteja de variables no usades degut al canvi

--- src/checks.c
+++ src/checks.c
@@ -6,32 +6,9 @@ int check_if_registered(char* ip){
     return r_findValue(ip);
 }
 
-int check_if_public_domain(char* req_domain){
-    int i; 
-    for (i=0; i < public_domains_num; i++){
-	if (0 == strcmp(req_domain, public_domains[i])){
-	    return 1;
-	}
-    }
-    return 0;
-}
-
-
-int check_if_private_domain(char* req_domain){
-    int i; 
-    for (i=0; i < private_domains_num; i++){
-	if (0 == strcmp(req_domain,private_domains[i])){
-	    return 1;
-	}
-    }
-    return 0;
-}
-
 int check_if_use_inite(){
     return use_inite;
 }
-
-
 
 
 /*
--- src/include/wrapp.h
+++ src/include/wrapp.h
@@ -6,24 +6,11 @@
 
 
 extern const unsigned int use_inite;
-
-extern const char *public_domains[]; 
-extern const char *private_domains[] ;
-
-extern int public_domains_num;
-extern int private_domains_num;
-
-
-extern const char public_ip[16];
 extern const char private_ip[16];
 extern const char comment[];
-
 extern const unsigned int max_forks;
-
 extern const char dhcp_ip_range[2][16];
-
-#define IP_INI dhcp_ip_range[0]
-
 extern const RR *records;
 
+#define IP_INI dhcp_ip_range[0]
 #endif
