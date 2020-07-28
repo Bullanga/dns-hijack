a5788a737ffbb6a9e8a23d586d05861f4cdfb27f
dhap0 <dhap0.infosec@gmail.com>
2020-07-28 23:20:01 +0200

    logica del processament de les respostes corregida

--- src/process.c
+++ src/process.c
@@ -3,6 +3,7 @@
 #include "process.h"
 #include "wrapp.h"
 #include "checks.h"
+#include "cdns_RR_t.h"
 
 //	void make_response(char* client_ip, char* req_domain, DNS_RR Request, const char *ip, const char *comment, int master_socket, const struct sockaddr client_addr, int client_len ){
 //	    if (check_if_private_domain(req_domain)){
@@ -18,17 +19,31 @@ int process (char* client_ip, char* req_domain, DNS_RR Request, const char *comm
 		char ip[16];		
 		int privat = resolve_query(req_domain, ip);
 
-    if (!check_if_use_inite() && ip != NULL){ //if inite not used, no more checks
-	    generate_success_response(Request, ip, comment, master_socket, client_addr, client_len);
+		// If inite is not used 
+    if (!check_if_use_inite())
+		{ 
+			if( ip == NULL) 
+				generate_failure_response( Request, master_socket, client_addr, client_len);
+			else 
+	    	generate_success_response(Request, ip, comment, master_socket, client_addr, client_len);
 			return 1;
     }
-    if (!check_if_registered(client_ip)){ //if not registered
-			if (privat)  //and going to private ip return 0.0.0.0
+
+		// If resource is marked as private and inite is up, ip must be registered
+		if (privat) 
+		{ 
+    	if (check_if_use_inite() && !check_if_registered(client_ip))
+			{ 
+				// Hook resolved ip to 0.0.0.0
 	    	generate_success_response(Request, "0.0.0.0", comment, master_socket, client_addr, client_len);
 				return 1;
+			}
     }
 
-	  generate_success_response(Request, ip, comment, master_socket, client_addr, client_len);
+		if( ip == NULL) 
+			generate_failure_response( Request, master_socket, client_addr, client_len);
+		else 
+	   	generate_success_response(Request, ip, comment, master_socket, client_addr, client_len);
 		return 1;
     
 }
