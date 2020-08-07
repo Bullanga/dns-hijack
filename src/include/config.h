505979e3a611c9bee52118bf4a93cfafa1ad3c11
xXIty <miquel.roset@fib.upc.edu>
2020-08-07 09:18:34 +0000

    add comment remembering about the format of the domain in an RR struct

--- src/include/config.h
+++ src/include/config.h
@@ -38,12 +38,14 @@ const  char  dhcp_ip_range[2][16]  =  {"192.168.1.1",  "192.168.1.200"};
 
 #define IP_INI dhcp_ip_range[0]
 
-const char inite_host[16]= "0.0.0.0";
+const char inite_host[16]= "192.168.1.35";
 
+const int RECORDS_SIZE = 2;
 const RR records[] = {
+  /* REMEMBER THAT THE DOMAIN SHOULD BE ENTERED WITH A DOT AT THE END */
   /*  domain            type  private  ip           */
-  {   "cpdvl-blau.vida",  1,    0,       "192.168.1.42"  },
-  {   "wikipedia.dks",  1,    1,       "127.0.0.1"  },
+  {   "cpdvl-blau.vida.",  1,    0,       "192.168.1.42"  },
+  {   "wikipedia.dks.",  1,    1,       "127.0.0.1"  },
 };
 
 //int r_length;
