7c2c90dcd3a86396a40d8a3b308e57497636231f
xXIty <miquel.roset@fib.upc.edu>
2020-08-11 10:44:42 +0000

    add missing change for the last commit

--- src/include/dnslib.h
+++ src/include/dnslib.h
@@ -5,8 +5,10 @@
 #include "dns_types.h"
 
 
-void generate_failure_response(Packet request, int master_socket, const struct sockaddr client_addr, int client_len);
-void generate_success_response(Packet request, const char *ip, const char *comment, int master_socket, const struct sockaddr client_addr, int client_len);
+void generate_failure_response(Message request, int master_socket, struct sockaddr client_addr, int client_len);
+
+void generate_failure_response(Message request, int master_socket, struct sockaddr client_addr, int client_len);
+void generate_success_response(Message request, const char *ip, const char *comment, int master_socket, struct sockaddr client_addr, int client_len);
 void parse_client_ip(char *target, const struct sockaddr *client);
 unsigned int parse_requested_domain(char *target, char *data);
 
