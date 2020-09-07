dd27f7bc517acb9acd9ed4eaa1a79fb4feb435d3
xXIty <miquel.roset@fib.upc.edu>
2020-09-07 12:51:08 +0200

    fix index out of boundary

--- src/guardaIP.c
+++ src/guardaIP.c
@@ -47,7 +47,7 @@ int calcRange(const char* ip_ini,const char* ip_fin){
 
 int r_findValue(char* ip){
    int index = calcIndex(ip);
-   if (index >= r_length) return -1;
+	 if (index >= r_length || index < 0) return -1;
    return registry[index] == 1; //em retornarà 1 si l'IP està registrada.
 }
 
